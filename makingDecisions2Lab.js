9b437f1b8e07c955ff5449038d5e69da03dffc61
Garrett Pace <elderpace6@gmail.com>
2023-04-13 15:31:42 -0600

    prob8

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -6,7 +6,7 @@ var arr = [10,20,30,40,50,60]
   Create a variable named 'firstItem' and set it equal to the first value of the 'arr' array.
 */
 
-let firstItem = arr.shift()
+let firstItem = arr[0]
 console.log(firstItem)
 
 
@@ -29,7 +29,8 @@ var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
   Loop through the 'family' array and console.log every item.
 */
 
-//Code Here
+for (let i = 0; i<family.length; i++)
+console.log(family[i])
 
 
 
@@ -44,7 +45,12 @@ let evensArr = []
   Use a for-loop to iterate through each number in the 'nums' array, if the number is even, append it to the 'evensArr'.
 */
 
-//Code Here
+for (let i = 0; i<nums.length; i++){
+  if(nums[i] % 2 ===0){
+    evensArr.push(nums[i])
+     }
+}
+console.log(evensArr)
 
 
 
@@ -55,10 +61,22 @@ var score = 74
 // Do not edit the code above.
 
 /*
-  Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, console log an 'A', between 80 and 89, console log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
+  Determine if the letter grade of the given variable 'score'.
+  If the variable is a 90 or above, console log an 'A', between 80 and 89, console log a 'B', between 70 and 79,
+  'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
 */
 
-//Code Here
+if(score >=90){
+  console.log("A")
+}else if(score >=80){
+  console.log("B")
+}else if(score >=70){
+  console.log("C")
+}else if(score >=60){
+  console.log("D")
+}else{
+  console.log("F")
+}
 
 
 ////////// Intermediate Problems //////////
@@ -73,15 +91,26 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
   Create a variable named 'someNum' and set it equal to the fifth value of the 'myFavoriteNumbers' array.
 */
 
-//Code Here
-
+let someNum = myFavoriteNumbers[4]
+console.log(someNum)
 
 ////////// PROBLEM 7 //////////
 
-// Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element, but there are not 7 elements in the array? Let's write some code to check for that. 
-// Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
+// Subscripting (accessing values using their index) an array can fail.
+// What happens if you subscript to the 7th element, but there are not 7 elements in the array?
+// Let's write some code to check for that. 
+// Use an if statement to check the length of the 'myFavoriteNumbers' array.
+// If it's less than 7, log 'There are not enough elements in this array' to the console.
+// If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array.
+// (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
-//Code Here
+if(myFavoriteNumbers.length < 7){
+  console.log('There are not enough elements in this array')
+}else {
+    someNum = myFavoriteNumbers[6]
+    console.log(someNum)
+}
+
 
 
 ////////// PROBLEM 8 //////////
@@ -92,7 +121,11 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
 
-//Code Here
+for (i=0; i<listOfNumbers.length; i++){
+  if(listOfNumbers[i] % 3 ===0){
+    console.log(`${listOfNumbers[i]} is divisible by 3.`)
+     }
+}
 
 
 ////////// PROBLEM 9 //////////
